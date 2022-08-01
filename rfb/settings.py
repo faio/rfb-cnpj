88b0c86d8ac6d4a8d470eab8c9ee94f40e4da1a1
Flávyo Henrique <fhms200@gmail.com>
2022-08-01 11:39:05 -0300

    - Voltado para a codificação iso-8859-1, deu problema em alguns registros em alguns sgbd

--- rfb/settings.py
+++ rfb/settings.py
@@ -1,5 +1,5 @@
 # Encoding do arquivo recebido pela RFB e do SGBD que será criado
-ENCODING = 'UTF8'
+ENCODING = 'ISO-8859-1'
 
 # Tamanho dos buffer no momento de persistir os dados no SGBD
 CHUNK_ROWS_INSERT_DATABASE = 20_000
